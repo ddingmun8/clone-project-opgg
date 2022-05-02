0a02f947c213549529878e9e4523981a17e18352
Gnar <ddingmun8@gmail.com>
2022-05-02 20:50:40 +0900

    main 수정1

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,7 +1,9 @@
-/*!
-* Start Bootstrap - Landing Page v6.0.5 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+alert("확인2");
+
+document.getElementById("btnSubmit").onclick = function() {
+    alert("클릭함");
+};
+
+$(document).on("click", "#btnSubmit", function(){
+    alert("btnSubmit 버튼 클릭 이벤트2");
+});
